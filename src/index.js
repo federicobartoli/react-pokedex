4b9f2d26acb2efa19be667affe65386aad118b2a
FedericoBartoli <federico.bartoli@exelab.com>
2021-09-29 17:37:30 +0200

    Fix issues

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import BrowserRouter from "react-router-dom/BrowserRouter";
 
 //Context
 import AllPokemonContextProvider from "./context/AllPokemonContext";
@@ -12,9 +11,7 @@ ReactDOM.render(
   <React.StrictMode>
     <AllPokemonContextProvider>
       <LoadMoreContextProvider>
-        <BrowserRouter basename={process.env.PUBLIC_URL}>
-          <App />
-        </BrowserRouter>
+        <App />
       </LoadMoreContextProvider>
     </AllPokemonContextProvider>
   </React.StrictMode>,
